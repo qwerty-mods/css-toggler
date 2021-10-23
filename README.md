b15f3f8000ce5912839fdd0d81f29d168c4c75ef
12944qwerty <qwerty12944qwerty@gmail.com>
2021-10-23 18:50:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSS Toggler
+
+Allows for more quick css functionality.
+Including:
+- Enabling and Disabling the snippet without removing it from the tab
+- Deleting the snippets via the message or command
+- Naming snippets
---
+++ index.js
@@ -0,0 +1,14 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class CSSToggler extends Plugin {
+    startPlugin() {
+        // TODO: Deleting snippet via message and command
+        // TODO: Enabling snippets via command or settings
+        // TODO: Disabling ^^
+        // TODO: Name different snippets differently (and potentially descriptions) for easier access
+    }
+
+    pluginWillUnload() {
+        
+    }
+}
