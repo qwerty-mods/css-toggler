be069dbb95db950d2d09e2b0fe62b1f21d52b519
GriefMoDz <griefmodz@gmail.com>
2021-10-27 00:05:45 +1100

    Add more preparations to 'SnippetCard'

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -62,12 +62,12 @@ module.exports = class Settings extends React.Component {
         <SnippetCard
           name='Unnamed Snippet #1'
           id='851188997115478026'
-          content={'.icon-27yU2q[src=\'https://cdn.discordapp.com/icons/538759280057122817/22db4d5a52d731f3b14729c079eac801.webp?size=128\'] {\ncontent:url(\'https://cdn.discordapp.com/avatars/518171798513254407/665fe74ee3fbf58b9d5949d7e15dbafa.webp?size=128\')\n}'}
+          content={'.icon-27yU2q[src=\'https://cdn.discordapp.com/icons/538759280057122817/22db4d5a52d731f3b14729c079eac801.webp?size=128\'] {\ncontent: url(\'https://cdn.discordapp.com/avatars/518171798513254407/665fe74ee3fbf58b9d5949d7e15dbafa.webp?size=128\');\n}'}
           description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         />
         <SnippetBox
-          name='Unnamed Snippet #1'
-          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          name='Hi'
+          description='No'
           enabled={false}
         >
         </SnippetBox>
--- components/SnippetCard.jsx
+++ components/SnippetCard.jsx
@@ -1,31 +1,63 @@
-const { React, getModule } = require('powercord/webpack');
+const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
 
+const { default: Button } = getModule(m => m.ButtonLink, false);
+const { default: Avatar } = getModule([ 'AnimatedAvatar' ], false);
+
+const { DEFAULT_AVATARS } = getModule([ 'getDefaultAvatarURL' ], false);
+
+const TextInput = getModuleByDisplayName('TextInput', false);
 const parser = getModule([ 'parse', 'parseTopic' ], false);
 
+// TODO: Replace placeholders with actual prop values.
 module.exports = React.memo(props => {
+  const [ title, setTitle ] = React.useState(props.name);
+
   return (
     <div className='css-toggler-snippet-card'>
-      <div className='css-toggler-snippet-card-header'>
-        <div className='css-toggler-snippet-card-header-title'>
-          {props.name}
-
-          <div className='css-toggler-snippet-card-header-title-right'>
-            ID: {props.id}
+      <div className='card-header'>
+        <div className='card-header-title'>
+          <TextInput
+            size='mini'
+            maxLength={32}
+            value={title || 'Unnamed Snippet'}
+            placeholder='Enter a title'
+            className='card-header-title-input'
+            inputClassName='card-header-title-input-box'
+            onChange={setTitle}
+          />
+          <div className='card-header-title-placeholder'>
+            {title}
           </div>
+        </div>
+
+        <div className='card-header-snippet-id'>
+          ID: {props.id}
         </div>
       </div>
 
-      <div className='css-toggler-snippet-card-body'>
-        {props.description !== null && <div className='css-toggler-snippet-card-body-description'>
+      <div className='card-body'>
+        {props.description !== null && <div className='card-body-description'>
           {props.description}
         </div>}
-        <div className='css-toggler-snippet-card-body-content'>
+        <div className='card-body-content'>
           {parser.reactParserFor(parser.defaultRules)(`\`\`\`css\n${props.content}\n\`\`\``)}
         </div>
       </div>
 
-      <div className='css-toggler-snippet-card-footer'>
-        <div className='css-toggler-snippet-card-footer-right'>
+      <div className='card-footer'>
+        <div className='card-footer-author'>
+          <div className='card-footer-author-avatar'>
+            <Avatar size={Avatar.Sizes.SIZE_32} src={DEFAULT_AVATARS[Math.floor(Math.random() * 5) % DEFAULT_AVATARS.length]}></Avatar>
+          </div>
+          <div className='card-footer-author-name'>
+            Discord#0000
+          </div>
+        </div>
+
+        <div className='card-footer-actions'>
+          <Button size={Button.Sizes.SMALL} color={Button.Colors.GREEN}>Edit</Button>
+          <Button size={Button.Sizes.SMALL} color={Button.Colors.RED}>Remove</Button>
+          <Button size={Button.Sizes.SMALL} color={Button.Colors.BRAND}>Disable</Button>
         </div>
       </div>
     </div>
