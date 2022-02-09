7e8592e6d4828a3a7e8db47a4631f68cc42243dc
12944qwerty <qwerty12944qwerty@gmail.com>
2022-02-08 20:24:58 -0600

    Make CSS Toggler Icon

--- index.jsx
+++ index.jsx
@@ -82,7 +82,9 @@ module.exports = class CSSToggler extends Plugin {
           header.props.children[1].props.children.unshift(
             <Tooltip text={Messages.CSS_TOGGLER_GO_TO_CSS_TOGGLER_TOOLTIP} position='left'>
               <Clickable onClick={() => settingsModule.open(this.entityID)} className='button'>
-                <Icon className='css-toggler-jump-icon' name='Reply' />
+                <svg className='css-toggler-jump-icon' viewBox='0 0 24 24' width="24">
+                  <path fill="currentColor" d="M19 18c0 1.104-.896 2-2 2s-2-.896-2-2 .896-2 2-2 2 .896 2 2zm-14-3c-1.654 0-3 1.346-3 3s1.346 3 3 3h14c1.654 0 3-1.346 3-3s-1.346-3-3-3h-14zm19 3c0 2.761-2.239 5-5 5h-14c-2.761 0-5-2.239-5-5s2.239-5 5-5h14c2.761 0 5 2.239 5 5zm0-12c0 2.761-2.239 5-5 5h-14c-2.761 0-5-2.239-5-5s2.239-5 5-5h14c2.761 0 5 2.239 5 5zm-15 0c0-1.104-.896-2-2-2s-2 .896-2 2 .896 2 2 2 2-.896 2-2z" />
+                </svg>
               </Clickable>
             </Tooltip>
           );
